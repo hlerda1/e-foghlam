11765c723e7fdf02225b64421980a1a8f23f6fb5
Horacio Lerda <41090380+hlerda1@users.noreply.github.com>
2024-03-11 14:51:38 -0300

    025 - Navbar  - colapso y ajuste tamaño cuando en mobile

--- src/efoghlam/components/NavBar.jsx
+++ src/efoghlam/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useAuthStore } from '../../hooks/useAuthStore';
 import { Link, NavLink } from 'react-router-dom';
+import {Navbar,Nav,Container,NavDropdown} from "react-bootstrap";
 
 export const NavBar = () => {
   const { startLogout, user } = useAuthStore();
@@ -11,7 +12,6 @@ export const NavBar = () => {
   if (localStorage.getItem('user')) {
     currentUserRole = JSON.parse(localStorage.getItem('user'));
   }
-  // const [show, setShow] = useState(currentUserRole === "Docente"); // Initialize show based on user role
 
   if (currentUserRole === 'docente') {
     x = true;
@@ -21,82 +21,48 @@ export const NavBar = () => {
   }, []);
 
   return (
-    <div className='navbar navbar-expand navbar-dark bg-dark mb px-4'>
-      <span className='navbar-brand'>
-        <img src="src/images/Foghlan_logo_trans.png" alt="Logo" width={40} height={40}/>
-        &nbsp;
-        {/* <i className='fa-solid fa-chalkboard-user'></i>
-        &nbsp; */}
-        {user.nombre}
-      </span>
 
-      <div className='container-fluid'>
-        <div className='collapse navbar-collapse' id='navbarNav'>
-          <ul className='navbar-nav'>
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? 'active' : ''}`
-                }
-                to='/auth/login'
-              >
-                Inicio
-              </NavLink>
-            {show ? (
-              <NavLink
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? 'active' : ''}`
-                }
-                to='/comisiones'
-              >
-                Comisiones
-              </NavLink>
-            ) : null}
-            <NavLink
-              className={({ isActive }) =>
-                `nav-link ${isActive ? 'active' : ''}`
-              }
-              to='/calendario'
-            >
-              Calendario
-            </NavLink>
+    <Navbar  collapseOnSelect expand="lg" bg="dark" variant="dark" className='space '>
+      {/* <Container > */}
+        <Navbar.Brand className='text-size'>
+          <span 
+          // className='navbar-brand'
+          >
+            <img src="src/images/Foghlan_logo_trans.png" alt="Logo" width={40} height={40}/>
+              &nbsp;
+            {user.nombre}
+          </span>
+        </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+               <div className='navbar navbar-expand navbar-dark bg-dark mb px-4'> 
+                <div className='container-fluid'>
+                  <div className='collapse navbar-collapse' id='navbarNav'>
+                    <ul className='navbar-nav'>
+                        <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/auth/login'>Inicio</NavLink>
+                      {show ? (
+                        <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/comisiones'>Comisiones</NavLink>
+                      ) : null}
+                      <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/calendario'>Calendario</NavLink>
+                      {show ? (
+                      <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/actividades'>Actividades</NavLink>
+                      ) : null}
+                      <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/notas'>Notas</NavLink>
+                      <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/comunicaciones'>Comunicaciones</NavLink>
+                      
+                      <button onClick={startLogout} className='btn btn-outline-danger'>
+                        <i className='fas fa-sign-out alt'></i>
+                        &nbsp;
+                        <span>Salir</span>
+                      </button>
 
-            {show ? (
-            <NavLink
-              className={({ isActive }) =>
-                `nav-link ${isActive ? 'active' : ''}`
-              }
-              to='/actividades'
-            >
-              Actividades
-            </NavLink>
-            ) : null}
-
-            <NavLink
-              className={({ isActive }) =>
-                `nav-link ${isActive ? 'active' : ''}`
-              }
-              to='/notas'
-            >
-              Notas
-            </NavLink>
-
-            <NavLink
-              className={({ isActive }) =>
-                `nav-link ${isActive ? 'active' : ''}`
-              }
-              to='/comunicaciones'
-            >
-              Comunicaciones
-            </NavLink>
-          </ul>
-        </div>
-      </div>
-
-      <button onClick={startLogout} className='btn btn-outline-danger'>
-        <i className='fas fa-sign-out alt'></i>
-        &nbsp;
-        <span>Salir</span>
-      </button>
-    </div>
+                    </ul>
+                  </div>
+                </div>
+              
+            </div>
+          </Navbar.Collapse>
+        {/* </Container> */}
+      </Navbar>
   );
 };
