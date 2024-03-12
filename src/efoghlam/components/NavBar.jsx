2f83e53c00fc710e9c7d816cb4cd23ecfca5578b
Horacio Lerda <41090380+hlerda1@users.noreply.github.com>
2024-03-12 12:34:00 -0300

    029a - Navbar - optimización mobile

--- src/efoghlam/components/NavBar.jsx
+++ src/efoghlam/components/NavBar.jsx
@@ -78,12 +78,14 @@ export const NavBar = () => {
       {['lg'].map((expand) => (
         <Navbar key={expand} expand={expand} className="bg-dark mb-3" variant="dark">
           <Container fluid>
-            <Navbar.Brand href="/auth/login">
-              <span>
-                <img src="public/Images/Foghlan_logo_trans.png" alt="Logo" width={40} height={40}/>
-                  &nbsp;
-                {user.nombre}
-              </span>
+            <Navbar.Brand>
+              <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/auth/login'>
+                <span>
+                  <img src="public/Images/Foghlan_logo_trans.png" alt="Logo" width={40} height={40}/>
+                    &nbsp;
+                  {user.nombre}
+                </span>
+              </NavLink>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
@@ -98,11 +100,11 @@ export const NavBar = () => {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  {show ? (<Nav.Link href="/comisiones">Comisiones</Nav.Link>) : null}
-                  <Nav.Link href="/calendario">Calendario</Nav.Link>
-                  {show ? (<Nav.Link href="/actividades">Actividades</Nav.Link>) : null}
-                  <Nav.Link href="/notas">Notas</Nav.Link>
-                  <Nav.Link href="/comunicaciones">Comunicaciones</Nav.Link>
+                  {show ? (<NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/comisiones'>Comisiones</NavLink>) : null}
+                  <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/calendario'>Calendario</NavLink>
+                  {show ? (<NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/actividades'>Actividades</NavLink>) : null}
+                  <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/notas'>Notas</NavLink>
+                  <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`} to='/comunicaciones'>Comunicaciones</NavLink>
                   <button onClick={startLogout} className='btn btn-outline-danger'>
                        <i className='fas fa-sign-out alt'></i>
                         &nbsp;
